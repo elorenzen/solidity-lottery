361f17834237ad9c01d8fe73dcf40b8dc628e1dd
elorenzen <eric.lorenzen@gmail.com>
2019-08-11 19:18:52 -0600

    Setup compile and export

---
+++ compile.js
@@ -0,0 +1,9 @@
+const path = require('path'),
+      solc = require('solc'),
+        fs = require('fs');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+
+module.exports = solc.compile(source, 1);
